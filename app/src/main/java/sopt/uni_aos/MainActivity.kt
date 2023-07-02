30debf8a95c8d671923534d896c9a53d97ae472d
HaeTi <t9h9@naver.com>
2023-07-02 21:44:48 +0900

    #1 / ktlint 반영

--- app/src/main/java/sopt/uni_aos/MainActivity.kt
+++ app/src/main/java/sopt/uni_aos/MainActivity.kt
@@ -1,7 +1,7 @@
 package sopt.uni_aos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
