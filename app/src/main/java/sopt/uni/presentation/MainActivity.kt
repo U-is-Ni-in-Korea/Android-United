fe2ff0121ca32ac299e6ef380450ca44037efef0
HaeTi <t9h9@naver.com>
2023-07-06 01:37:13 +0900

    #4 / 임시 메인 페이지 생성

--- app/src/main/java/sopt/uni/presentation/MainActivity.kt
+++ app/src/main/java/sopt/uni/presentation/MainActivity.kt
@@ -5,11 +5,19 @@ import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
 import sopt.uni.databinding.ActivityMainBinding
 import sopt.uni.util.binding.BindingActivity
+import sopt.uni.util.extension.startActivity
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        moveToPage()
+    }
+
+    private fun moveToPage() {
+        binding.btnOnBoarding.setOnClickListener {
+            startActivity<OnBoardingActivity>()
+        }
     }
 }
