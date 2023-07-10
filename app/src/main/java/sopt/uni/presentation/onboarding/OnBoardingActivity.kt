789081fda1bfe4cad0fded8c3f86d15c456e21d4
HaeTi <t9h9@naver.com>
2023-07-10 18:25:18 +0900

    #12 / indentation 변경

--- app/src/main/java/sopt/uni/presentation/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/sopt/uni/presentation/onboarding/OnBoardingActivity.kt
@@ -28,16 +28,18 @@ class OnBoardingActivity :
     }
 
     private fun changeSkipText() {
-        binding.vpOnBoarding.registerOnPageChangeCallback(object :
-            ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                if (position == 2) {
-                    binding.tvSkip.text = getString(R.string.onboading_start)
-                } else {
-                    binding.tvSkip.text = getString(R.string.onboarding_skip)
+        binding.vpOnBoarding.registerOnPageChangeCallback(
+            object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (position == 2) {
+                        binding.tvSkip.text = getString(R.string.onboading_start)
+                    } else {
+                        binding.tvSkip.text = getString(R.string.onboarding_skip)
+                    }
                 }
-            }
-        })
+            },
+        )
     }
 }
