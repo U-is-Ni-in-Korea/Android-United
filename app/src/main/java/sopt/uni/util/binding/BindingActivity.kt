c4e71a8a6973ed58641db00b40841907b1643823
HaeTi <t9h9@naver.com>
2023-07-10 11:49:19 +0900

    #1 / BindingActivity에 키보드 제거 함수 추가

--- app/src/main/java/sopt/uni/util/binding/BindingActivity.kt
+++ app/src/main/java/sopt/uni/util/binding/BindingActivity.kt
@@ -7,8 +7,7 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.google.android.material.internal.ViewUtils.hideKeyboard
-
+import sopt.uni.util.extension.hideKeyboard
 abstract class BindingActivity<T : ViewDataBinding>(
     @LayoutRes private val layoutRes: Int,
 ) : AppCompatActivity() {
