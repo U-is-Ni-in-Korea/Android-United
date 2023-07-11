996d410526dce0e79bde888a74ed662a98547ff4
HaeTi <t9h9@naver.com>
2023-07-11 15:25:28 +0900

    #11 / 액티비티에 hilt 적용

--- app/src/main/java/sopt/uni/presentation/invite/InviteHubActivity.kt
+++ app/src/main/java/sopt/uni/presentation/invite/InviteHubActivity.kt
@@ -1,10 +1,12 @@
 package sopt.uni.presentation.invite
 
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
 import sopt.uni.databinding.ActivityInviteHubBinding
 import sopt.uni.util.binding.BindingActivity
 
+@AndroidEntryPoint
 class InviteHubActivity : BindingActivity<ActivityInviteHubBinding>(R.layout.activity_invite_hub) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
