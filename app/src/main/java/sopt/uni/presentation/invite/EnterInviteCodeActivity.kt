a1ba61e1a78cba6f62cc997dd2c3675718049a3f
HaeTi <t9h9@naver.com>
2023-07-11 15:25:28 +0900

    #11 / 액티비티에 hilt 적용

--- app/src/main/java/sopt/uni/presentation/invite/EnterInviteCodeActivity.kt
+++ app/src/main/java/sopt/uni/presentation/invite/EnterInviteCodeActivity.kt
@@ -1,10 +1,12 @@
 package sopt.uni.presentation.invite
 
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
 import sopt.uni.databinding.ActivityEnterInviteCodeBinding
 import sopt.uni.util.binding.BindingActivity
 
+@AndroidEntryPoint
 class EnterInviteCodeActivity :
     BindingActivity<ActivityEnterInviteCodeBinding>(R.layout.activity_enter_invite_code) {
     override fun onCreate(savedInstanceState: Bundle?) {
