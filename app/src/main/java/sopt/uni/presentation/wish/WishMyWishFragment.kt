86ddbaeb3a1942b3f4c118534e8eb31cd1eacc82
sub101 <subin10@nate.com>
2023-07-12 17:12:37 +0900

    #19 / 줄 정렬

--- app/src/main/java/sopt/uni/presentation/wish/WishMyWishFragment.kt
+++ app/src/main/java/sopt/uni/presentation/wish/WishMyWishFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import sopt.uni.R
 import sopt.uni.data.entity.wish.MULTI_TYPE_WISH1
