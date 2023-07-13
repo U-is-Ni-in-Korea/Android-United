83b6f29b65fc007989c14693b257c3a21feebf39
taeheeL <t9h9@soongsil.ac.kr>
2023-07-13 21:46:11 +0900

    #17 / 1

---
+++ app/src/main/java/sopt/uni/presentation/MainHistoryActivity.kt
@@ -0,0 +1,12 @@
+package sopt.uni.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sopt.uni.R
+
+class MainHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_history)
+    }
+}
---
+++ app/src/main/java/sopt/uni/presentation/SubHistoryActivity.kt
@@ -0,0 +1,12 @@
+package sopt.uni.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sopt.uni.R
+
+class SubHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_history)
+    }
+}
