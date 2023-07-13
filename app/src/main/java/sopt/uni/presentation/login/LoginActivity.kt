afad5f0829a55705c069186728f475e6ccfa803d
HaeTi <t9h9@naver.com>
2023-07-13 12:31:25 +0900

    #26 / 구글 로그인 구현 (인가코드 받기)

--- app/src/main/java/sopt/uni/presentation/login/LoginActivity.kt
+++ app/src/main/java/sopt/uni/presentation/login/LoginActivity.kt
@@ -1,12 +1,23 @@
 package sopt.uni.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import sopt.uni.BuildConfig
 import sopt.uni.R
 import sopt.uni.data.service.KakaoLoginService
 import sopt.uni.databinding.ActivityLoginBinding
@@ -22,6 +33,11 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
     @Inject
     lateinit var kakaoLoginService: KakaoLoginService
     private val loginViewModel by viewModels<LoginViewModel>()
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var startGoogleLoginForResult: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -29,7 +45,11 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
         val keyHash = Utility.getKeyHash(this)
         Timber.d(keyHash)
 
+        auth = FirebaseAuth.getInstance()
+        googleInit()
+
         moveToKakaoLogin()
+        moveToGoogleLogin()
         collectIsTokenAvailability()
     }
 
@@ -52,12 +72,66 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
     }
 
     private fun moveToKakaoLogin() {
-        binding.ivKakaoLogin.setOnSingleClickListener {
+        binding.llKakaoLogin.setOnSingleClickListener {
             if (kakaoLoginService.isKakaoTalkLoginAvailable) {
                 kakaoLoginService.loginByKakaoTalk()
             } else {
                 kakaoLoginService.loginByKakaoAccount()
             }
+        }
+    }
+
+    // TODO 함수 분리
+    private fun googleInit() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.GOOGLE_CLIENT_ID)
+            .requestEmail()
+            .requestId()
+            .requestProfile()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        startGoogleLoginForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == RESULT_OK) {
+                    result.data?.let { data ->
+
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+                        try {
+                            val account = task.getResult(ApiException::class.java)!!
+                            Timber.e("firebaseAuthWithGoogle:" + account.id)
+                            firebaseAuthWithGoogle(account.idToken!!)
+                        } catch (e: ApiException) {
+                            Timber.e("Google sign in failed", e)
+                        }
+                    }
+                } else {
+                    Timber.e("Google Result Error $result")
+                }
+            }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Timber.e("signInWithCredential:success")
+                    startActivity<NickNameActivity>()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Timber.e("signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun moveToGoogleLogin() {
+        binding.llGoogleLogin.setOnSingleClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startGoogleLoginForResult.launch(signInIntent)
         }
     }
 }
