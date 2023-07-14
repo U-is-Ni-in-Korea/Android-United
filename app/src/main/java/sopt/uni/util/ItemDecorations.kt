6fc12184bcf54a7d80d425e657a93e48d3a8d170
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-14 16:12:45 +0900

    Add ItemDecorations

---
+++ app/src/main/java/sopt/uni/util/ItemDecorations.kt
@@ -0,0 +1,33 @@
+package sopt.uni.util
+
+import android.content.res.Resources
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.roundToInt
+
+class ItemDecorations(
+    private val topSpace: Int,
+    private val bottomSpace: Int,
+    private val leftSpace: Int,
+    private val rightSpace: Int
+) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        outRect.top = dpToPx(topSpace)
+        outRect.bottom = dpToPx(bottomSpace)
+        outRect.left = dpToPx(leftSpace)
+        outRect.right = dpToPx(rightSpace)
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        val density = Resources.getSystem().displayMetrics.density
+        return (dp * density).roundToInt()
+    }
+}
