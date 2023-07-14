6da81b7229fa28e6bb7e3b9d52fc997242f3f6d9
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-14 16:12:26 +0900

    Add MissionCategoryAdapter

---
+++ app/src/main/java/sopt/uni/presentation/shortgame/MissionViewHolder.kt
@@ -0,0 +1,37 @@
+package sopt.uni.presentation.shortgame
+
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import sopt.uni.R
+import sopt.uni.data.entity.shortgame.Mission
+import sopt.uni.databinding.ItemMissionBinding
+import sopt.uni.presentation.entity.MissionIdPosition
+
+class MissionViewHolder(val binding: ItemMissionBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun onBind(
+        data: Mission,
+        isSelected: Boolean,
+        position: Int,
+        goToMissionDetailClickListener: (MissionIdPosition) -> Unit,
+        selectMissionClickListener: (MissionIdPosition) -> Unit
+    ) {
+
+        binding.apply {
+            ivMission.load(data.image)
+            tvMissionTitle.text = data.title
+            ivMissionDetail.setOnClickListener {
+                goToMissionDetailClickListener(MissionIdPosition(data.id, position))
+            }
+            root.setOnClickListener {
+                selectMissionClickListener(MissionIdPosition(data.id, position))
+            }
+        }
+
+        if (isSelected)
+            binding.cvBackground.setBackgroundResource(R.drawable.bg_mission_hover)
+        else
+            binding.cvBackground.setBackgroundResource(0)
+    }
+}
