1d442a0a8169c1d329fcda2f80a0a1210268a23f
l2zh <dlwnsgml0109@naver.com>
2023-07-15 02:41:20 +0900

    #32 / 바인딩 다이얼로그 프레그먼트 구현, 사용 예시(history)

--- app/src/main/java/sopt/uni/presentation/HistorySubActivity.kt
+++ app/src/main/java/sopt/uni/presentation/HistorySubActivity.kt
@@ -6,14 +6,14 @@ import sopt.uni.R
 import sopt.uni.databinding.ActivitySubHistoryBinding
 import sopt.uni.util.binding.BindingActivity
 
-class SubHistoryActivity :
+class HistorySubActivity :
     BindingActivity<ActivitySubHistoryBinding>(R.layout.activity_sub_history) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.historySubBack.setOnClickListener {
-            val intent = Intent(this, MainHistoryActivity::class.java)
+            val intent = Intent(this, HistoryMainActivity::class.java)
             startActivity(intent)
             finish()
         }
