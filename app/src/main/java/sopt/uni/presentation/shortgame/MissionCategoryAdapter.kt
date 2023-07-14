fc9a758574b80982ccece897661846ff3fdbe458
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-14 16:12:26 +0900

    Add MissionCategoryAdapter

---
+++ app/src/main/java/sopt/uni/presentation/shortgame/MissionCategoryAdapter.kt
@@ -0,0 +1,43 @@
+package sopt.uni.presentation.shortgame
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import sopt.uni.data.entity.shortgame.Mission
+import sopt.uni.databinding.ItemMissionBinding
+import sopt.uni.presentation.entity.MissionIdPosition
+import sopt.uni.util.extension.ItemDiffCallback
+
+class MissionCategoryAdapter(
+    private val goToMissionDetailClickListener: (MissionIdPosition) -> Unit,
+    private val selectMissionClickListener: (MissionIdPosition) -> Unit
+) : ListAdapter<Mission, MissionViewHolder>(
+    ItemDiffCallback<Mission>(
+        onContentsTheSame = { old, new -> old == new },
+        onItemsTheSame = { old, new -> old == new },
+    ),
+) {
+    private var selectedPosition = 0;
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MissionViewHolder {
+        val binding =
+            ItemMissionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MissionViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MissionViewHolder, position: Int) {
+        holder.onBind(
+            getItem(position),
+            position == selectedPosition,
+            position,
+            goToMissionDetailClickListener,
+            selectMissionClickListener
+        )
+    }
+
+    fun setSelectedItem(position: Int) {
+        val prevPosition = selectedPosition
+        selectedPosition = position
+        notifyItemChanged(prevPosition)
+        notifyItemChanged(selectedPosition)
+    }
+}
