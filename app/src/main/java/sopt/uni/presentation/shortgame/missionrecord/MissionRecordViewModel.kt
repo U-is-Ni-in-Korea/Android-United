88c334f1de6cb224899d4270f1d86b55a9f5398c
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-15 17:15:06 +0900

    #36 / Add MissionREcordViewModel

--- app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordViewModel.kt
@@ -4,22 +4,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MissionRecordViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
-    val roundGameId: Int = savedStateHandle.get<Int>(MissionRecordActivity.ROUND_GAME_ID) ?: -1
+class MissionRecordViewModel(
+        private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    val roundGameId: Int = savedStateHandle.get<Int>(MissionRecordActivity.ROUND_GAME_ID)
+            ?: -1
     val missionId = MutableLiveData<Int>(1)
     val missionImage = MutableLiveData<String>("")
     val missionContent = MutableLiveData<String>("헤드셋")
     val missionTitle = MutableLiveData<String>("키워드 스무고개")
 
     fun loadMissionRecord() {
-        // TODO: Get 승부기록뷰 정보
+        // TODO : Get 승부기록뷰 정보
     }
 
     fun requestMission(missionRequest: Boolean) {
-        // TODO: 미션완료,실패 API 호출
+        // TODO : 미션완료,실패 API 호출
     }
 
     fun requestStopMission() {
-        // TODO: 승부그만두기 API 호출
+        // TODO : 승부그만두기 API 호출
     }
 }
