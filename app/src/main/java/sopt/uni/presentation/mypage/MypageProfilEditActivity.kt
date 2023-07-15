dbb9e60bc28c838d5c069abcfb9f01ccef5524b9
l2zh <dlwnsgml0109@naver.com>
2023-07-15 22:43:19 +0900

    #38 / 마이페이지 프로필 수정 닉네임 뷰 기능 구현 완료

--- app/src/main/java/sopt/uni/presentation/mypage/MypageProfilEditActivity.kt
+++ app/src/main/java/sopt/uni/presentation/mypage/MypageProfilEditActivity.kt
@@ -1,12 +1,58 @@
 package sopt.uni.presentation.mypage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.databinding.BindingAdapter
 import sopt.uni.R
+import sopt.uni.databinding.ActivityMypageProfilEditBinding
+import sopt.uni.util.binding.BindingActivity
 
-class MypageProfilEditActivity : AppCompatActivity() {
+class MypageProfilEditActivity :
+    BindingActivity<ActivityMypageProfilEditBinding>(R.layout.activity_mypage_profil_edit) {
+
+    private val viewModel: MypageProfilEditViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage_profil_edit)
+        setContentView(binding.root)
+        binding.viewModel = viewModel
+    }
+
+    companion object {
+        private const val MAX_LENGTH = 10
+
+        @JvmStatic
+        @BindingAdapter("setMypageContentLength")
+        fun setMypageContentLength(view: TextView, length: Int) {
+            if (length >= MAX_LENGTH) {
+                view.setTextColor(view.context.getColor(R.color.Red_500))
+            } else {
+                view.setTextColor(view.context.getColor(R.color.Gray_400))
+            }
+            view.text = "$length/$MAX_LENGTH"
+        }
+
+        @JvmStatic
+        @BindingAdapter("setMypageErrorTextVisible")
+        fun setMypageErrorTextVisible(view: TextView, length: Int) {
+            if (length >= MAX_LENGTH) {
+                view.visibility = View.VISIBLE
+            } else {
+                view.visibility = View.INVISIBLE
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("setMypageContentLength")
+        fun setMypageContentLength(view: EditText, length: Int) {
+            if (length >= MAX_LENGTH) {
+                view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text_error)
+            } else {
+                view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text)
+            }
+        }
     }
 }
--- app/src/main/java/sopt/uni/presentation/shortgame/CreateShortGameActivity.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/CreateShortGameActivity.kt
@@ -29,7 +29,7 @@ class CreateShortGameActivity :
             },
             selectMissionClickListener = { missionIdPosition ->
                 selectItem(missionIdPosition.id, missionIdPosition.position)
-            }
+            },
         )
     }
 
