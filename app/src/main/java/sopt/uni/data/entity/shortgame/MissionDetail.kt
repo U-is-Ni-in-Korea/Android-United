03132eb18753ce40c0613de7b49f9290fb7635b5
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-15 17:46:45 +0900

    Fix ktlint

--- app/src/main/java/sopt/uni/data/entity/shortgame/MissionDetail.kt
+++ app/src/main/java/sopt/uni/data/entity/shortgame/MissionDetail.kt
@@ -4,7 +4,7 @@ data class MissionDetail(
     val id: Int,
     val title: String,
     val description: String,
-    val rule : String,
+    val rule: String,
     val tip: String,
     val image: String,
     val missionContentList: List<MissionContent>
