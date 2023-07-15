22def0263bfa3961a3dd728aa287fc5f5f226e0e
HaeTi <t9h9@naver.com>
2023-07-15 15:10:14 +0900

    #32 / WishActivity에 BindingActivty 적용

--- app/src/main/java/sopt/uni/presentation/wish/WishActivity.kt
+++ app/src/main/java/sopt/uni/presentation/wish/WishActivity.kt
@@ -1,17 +1,15 @@
 package sopt.uni.presentation.wish
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import sopt.uni.R
 import sopt.uni.databinding.ActivityWishBinding
+import sopt.uni.util.binding.BindingActivity
 
-class WishActivity : AppCompatActivity() {
-    lateinit var binding: ActivityWishBinding
+class WishActivity : BindingActivity<ActivityWishBinding>(R.layout.activity_wish) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWishBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_wish)
