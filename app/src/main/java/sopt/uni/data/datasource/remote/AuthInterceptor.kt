2910e959c0a21535a155ef518bdb5d4a9c3535a7
HaeTi <t9h9@naver.com>
2023-07-16 18:35:06 +0900

    #42 / AuthInterceptor를 통해 로컬에 토큰 저장

---
+++ app/src/main/java/sopt/uni/data/datasource/remote/AuthInterceptor.kt
@@ -0,0 +1,33 @@
+package sopt.uni.data.datasource.remote
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import sopt.uni.data.datasource.local.SparkleStorage
+
+class AuthInterceptor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = SparkleStorage.accessToken
+
+        val originalRequest = chain.request()
+
+        if (shouldSkipIntercept(originalRequest)) {
+            return chain.proceed(originalRequest)
+        }
+
+        val headerRequest = originalRequest.newBuilder()
+            .header(
+                "Authorization",
+                accessToken ?: return chain.proceed(originalRequest),
+            )
+            .build()
+        return chain.proceed(headerRequest)
+    }
+
+    private fun shouldSkipIntercept(request: Request): Boolean {
+        if (request.headers.size > 0) {
+            return true
+        }
+        return false
+    }
+}
