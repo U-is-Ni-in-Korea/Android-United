6d56cf78c889a4284f3bb2d6d77101b27f4aa7f4
HaeTi <t9h9@naver.com>
2023-07-16 18:34:21 +0900

    #42 / Bind AuthRepository

--- app/src/main/java/sopt/uni/data/repository/auth/AuthRepository.kt
+++ app/src/main/java/sopt/uni/data/repository/auth/AuthRepository.kt
@@ -1,5 +1,11 @@
 package sopt.uni.data.repository.auth
 
+import sopt.uni.data.entity.auth.Token
+
 interface AuthRepository {
-    suspend fun loginByKakao(token: String)
+    suspend fun getToken(social: String, code: String): Result<Token>
+
+    suspend fun initToken(accessToken: String, refreshToken: String)
+
+    suspend fun clearToken()
 }
---
+++ app/src/main/java/sopt/uni/data/repository/auth/AuthRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package sopt.uni.data.repository.auth
+
+import sopt.uni.data.datasource.remote.AuthDatasource
+import sopt.uni.data.entity.auth.Token
+import sopt.uni.data.source.remote.request.RequestAuthDto
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val authDatasource: AuthDatasource,
+) : AuthRepository {
+    override suspend fun getToken(social: String, code: String): Result<Token> =
+        kotlin.runCatching {
+            authDatasource.getToken(social, RequestAuthDto(code)).toToken()
+        }.onSuccess {
+            Result.success(it.toToken())
+        }.onFailure {
+            Result.failure<Token>(it)
+        }
+
+    override suspend fun initToken(accessToken: String, refreshToken: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearToken() {
+        TODO("Not yet implemented")
+    }
+}
