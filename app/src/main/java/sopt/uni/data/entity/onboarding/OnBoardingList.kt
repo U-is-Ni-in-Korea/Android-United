26579a69c3c58ec6341289a5ff5f7055892e963b
HaeTi <t9h9@naver.com>
2023-07-17 15:38:23 +0900

    #48 / 온보딩 이미지 추가

--- app/src/main/java/sopt/uni/data/entity/onboarding/OnBoardingList.kt
+++ app/src/main/java/sopt/uni/data/entity/onboarding/OnBoardingList.kt
@@ -4,17 +4,17 @@ import sopt.uni.R
 
 val onBoardingList = listOf<OnBoardingItem>(
     OnBoardingItem(
-        image = R.drawable.ic_onboarding,
+        image = R.drawable.onboarding1,
         title = "다양한 카테고리의 미션을 함께 즐겨보세요",
         description = "연인의 새로운 매력을 발견할 수 있어요",
     ),
     OnBoardingItem(
-        image = R.drawable.ic_onboarding,
+        image = R.drawable.onboarding2,
         title = "장기 승부와 한판 승부를 선택할 수 있어요",
         description = "설렘 가득 짜릿한 승부를 진행해 보세요",
     ),
     OnBoardingItem(
-        image = R.drawable.ic_onboarding,
+        image = R.drawable.onboarding3,
         title = "승부를 통해 우리 둘만의 소원권을 생성해보세요",
         description = "소중한 추억을 만들고 기록할 수 있어요",
     ),
