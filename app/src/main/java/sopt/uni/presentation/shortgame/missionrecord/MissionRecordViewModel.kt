267df195f00ea0466cb1e9e882881ad0a70d435d
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-19 03:17:33 +0900

    #57 / Update MissionRecordViewModel
    livedata dto 수정 및 데이터바인딩 수정

--- app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordViewModel.kt
@@ -1,25 +1,36 @@
 package sopt.uni.presentation.shortgame.missionrecord
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import sopt.uni.data.entity.shortgame.ResponseShortGameResult
 
 class MissionRecordViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     val roundGameId: Int = savedStateHandle.get<Int>(MissionRecordActivity.ROUND_GAME_ID) ?: -1
-    val missionId = MutableLiveData<Int>(1)
-    val missionImage = MutableLiveData<String>("")
-    val missionContent = MutableLiveData<String>("헤드셋")
-    val missionTitle = MutableLiveData<String>("키워드 스무고개")
+
+    private val _missionResult = MutableLiveData<ResponseShortGameResult>()
+    val missionResult: LiveData<ResponseShortGameResult> = _missionResult
+
+    private val _isMissionCancelSuccess = MutableLiveData<Boolean>(false)
+    val isMissionCancelSuccess = _isMissionCancelSuccess
+
+    private val _isMissionRequestSuccess = MutableLiveData<Boolean>(false)
+    val isMissionRequestSuccess = _isMissionRequestSuccess
+
+    val myMissionResult = missionResult.map { it.myRoundMission }
+    val missionId = myMissionResult.map { it.missionContent.id }
 
     fun loadMissionRecord() {
         // TODO: Get 승부기록뷰 정보
     }
 
     fun requestMission(missionRequest: Boolean) {
-        // TODO: 미션완료,실패 API 호출
+        _isMissionRequestSuccess.postValue(true)
     }
 
     fun requestStopMission() {
-        // TODO: 승부그만두기 API 호출
+        _isMissionCancelSuccess.postValue(true)
     }
 }
