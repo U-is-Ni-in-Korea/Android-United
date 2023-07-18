bf6f188724d3cece0dfe8f574a98dbc3aefdc3de
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-19 00:00:22 +0900

    #35 / Remove unused import

--- app/src/main/java/sopt/uni/presentation/shortgame/createshortgame/CreateShortGameViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/createshortgame/CreateShortGameViewModel.kt
@@ -1,6 +1,5 @@
 package sopt.uni.presentation.shortgame.createshortgame
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
