7f8525a5b4a11d1c77b6701536c69ecf077cc020
l2zh <dlwnsgml0109@naver.com>
2023-07-18 14:39:01 +0900

    # 49/ 토큰 삭제

--- app/src/main/java/sopt/uni/data/repository/history/HistoryRepositoryImpl.kt
+++ app/src/main/java/sopt/uni/data/repository/history/HistoryRepositoryImpl.kt
@@ -7,7 +7,7 @@ class HistoryRepositoryImpl(
     private val historyService: HistoryService,
 ) : HistoryRepository {
     override suspend fun getHistoryList(): Result<HistoryResponse> {
-        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhY2Nlc3NUb2tlbiIsImlhdCI6MTY4OTQ0ODI5MywiZXhwIjoxNjkxMjQ4MjkzLCJ1c2VySWQiOjV9.05nNO-UkGtlg2iCY22039x-TTaSZ0NLxkqHbqqnIpbYRiiJbW0nH-Ckt_x-tbRF1"
+        val token = ""
         return try {
             val historyResponse = historyService.getHistoryList("Bearer $token")
             Result.success(historyResponse)
