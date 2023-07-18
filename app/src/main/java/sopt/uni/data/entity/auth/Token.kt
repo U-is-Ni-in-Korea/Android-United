8566845b84548e23c7d0ce5e78bebb19ea2308c6
HaeTi <t9h9@naver.com>
2023-07-19 02:49:29 +0900

    #48 / 토큰 class에 함수 추가

--- app/src/main/java/sopt/uni/data/entity/auth/Token.kt
+++ app/src/main/java/sopt/uni/data/entity/auth/Token.kt
@@ -3,4 +3,6 @@ package sopt.uni.data.entity.auth
 data class Token(
     val accessToken: String = "",
     val refreshToken: String? = "",
-)
+) {
+    fun toToken(): Token = Token(accessToken = this.accessToken, refreshToken = this.refreshToken)
+}
