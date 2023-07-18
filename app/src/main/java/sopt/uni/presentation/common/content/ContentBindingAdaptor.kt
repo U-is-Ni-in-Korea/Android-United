2d2656ddadcb6e6caf91620e0aaad5ddc8f31972
HaeTi <t9h9@naver.com>
2023-07-19 04:19:59 +0900

    #48 / box stroke 상태 추가

--- app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
+++ app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
@@ -22,6 +22,8 @@ fun setNicknameErrorMessage(view: TextView, length: Int) {
 fun setNicknameContentLength(view: EditText, length: Int) {
     if (length > MAX_LENGTH) {
         view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text_error)
+    } else if (length == 0) {
+        view.background = view.context.getDrawable(R.drawable.bg_enter_edit_text_empty)
     } else {
         view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text)
     }
