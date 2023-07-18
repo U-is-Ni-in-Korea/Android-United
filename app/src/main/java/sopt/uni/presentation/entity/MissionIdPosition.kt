c830c473f482f4e76cedeb4dc2f7e11c03e41cdf
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-18 23:11:06 +0900

    #35 / Add MissionDetailCreateViewModel with Update MissionDetailCreateActivity

--- app/src/main/java/sopt/uni/presentation/entity/MissionIdPosition.kt
+++ app/src/main/java/sopt/uni/presentation/entity/MissionIdPosition.kt
@@ -1,6 +1,10 @@
 package sopt.uni.presentation.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class MissionIdPosition(
     val id: Int,
-    val position: Int
-)
+    val position: Int,
+) : Parcelable
