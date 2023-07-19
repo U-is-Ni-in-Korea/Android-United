92bddbad572a6b0eafb5ce5ce8bf18c376020bdb
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-19 16:02:14 +0900

    #61 / Refactor data class

--- app/src/main/java/sopt/uni/data/entity/shortgame/Mission.kt
+++
@@ -1,3 +0,0 @@
-package sopt.uni.data.entity.shortgame
-
-data class Mission(val id: Int, val title: String, val image: String)
--- app/src/main/java/sopt/uni/data/entity/shortgame/MissionDetail.kt
+++ app/src/main/java/sopt/uni/data/entity/shortgame/MissionDetail.kt
@@ -1,11 +1,22 @@
 package sopt.uni.data.entity.shortgame
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MissionDetail(
+    @SerialName("id")
     val id: Int,
+    @SerialName("title")
     val title: String,
+    @SerialName("description")
     val description: String,
+    @SerialName("rule")
     val rule: String,
+    @SerialName("tip")
     val tip: String,
+    @SerialName("image")
     val image: String,
-    val missionContentList: List<MissionExample>
+    @SerialName("missionContentList")
+    val missionContentList: List<MissionExample>,
 )
