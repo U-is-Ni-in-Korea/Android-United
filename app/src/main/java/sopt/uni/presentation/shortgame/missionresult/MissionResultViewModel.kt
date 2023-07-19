51f9a239974c7cb983942a841eb753ee513e9711
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-20 00:32:31 +0900

    #61 / Update MissionResult
    date 파싱 수정
    array resource 수정

--- app/src/main/java/sopt/uni/presentation/shortgame/missionresult/MissionResultViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/missionresult/MissionResultViewModel.kt
@@ -1,21 +1,51 @@
 package sopt.uni.presentation.shortgame.missionresult
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import sopt.uni.data.entity.shortgame.MissionResultState
-import sopt.uni.data.source.remote.response.ResponseShortGameResultDto
+import sopt.uni.data.entity.shortgame.RoundMission
+import sopt.uni.data.repository.shortgame.ShortGameRepository
 import sopt.uni.presentation.shortgame.missionrecord.MissionRecordActivity
+import javax.inject.Inject
 
-class MissionResultViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
-    // TODO: response 네이밍 바꾸고 Result로 감싸서 성공 실패 분기 처리하기
+@HiltViewModel
+class MissionResultViewModel @Inject constructor(
+    private val shortGameRepository: ShortGameRepository,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
     val roundGameId: Int = savedStateHandle.get<Int>(MissionRecordActivity.ROUND_GAME_ID) ?: -1
-    private val _missionResult = MutableLiveData<ResponseShortGameResultDto>()
-    val missionResult: LiveData<ResponseShortGameResultDto> = _missionResult
-    val myMissionResult = missionResult.map { it.myRoundMission }
-    val partnerMissionResult = missionResult.map { it.partnerRoundMission }
-    val myMissionResultState =
-        myMissionResult.map { MissionResultState.getMissionResultType(it.finalResult) }
+
+    private val _myMissionResult = MutableLiveData<RoundMission>()
+    val myMissionResult = _myMissionResult
+
+    private val _partnerMissionResult = MutableLiveData<RoundMission?>()
+    val partnerMissionResult = _partnerMissionResult
+
+    private val _myMissionResultState = MutableLiveData<MissionResultState>()
+    val myMissionResultState = _myMissionResultState
+
+    init {
+        setMissionRecord()
+    }
+
+    private fun setMissionRecord() {
+        viewModelScope.launch {
+            shortGameRepository.getShortGameResult(roundGameId).onSuccess {
+                _myMissionResult.value = it.myRoundMission
+                if (it.partnerRoundMission != null) {
+                    _partnerMissionResult.value =
+                        it.partnerRoundMission
+                }
+                _myMissionResultState.value =
+                    MissionResultState.getMissionResultType(it.myRoundMission.finalResult)
+            }.onFailure {
+                // TODO: 실패 로직 구현
+            }
+        }
+    }
 }
