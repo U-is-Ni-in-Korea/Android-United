92a51e71beb529f6df5c185f96143ee464a337ea
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-19 18:02:30 +0900

    #61 / Add CreateShortGame API

--- app/src/main/java/sopt/uni/data/service/ShortGameService.kt
+++ app/src/main/java/sopt/uni/data/service/ShortGameService.kt
@@ -1,9 +1,18 @@
 package sopt.uni.data.service
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import sopt.uni.data.entity.shortgame.MissionDetail
+import sopt.uni.data.source.remote.request.RequestCreateShortGameDto
+import sopt.uni.data.source.remote.response.ResponseCreateShortGameDto
 
 interface ShortGameService {
     @GET("/api/mission")
     suspend fun getMissionCategoryList(): List<MissionDetail>
+
+    @POST("/api/game/short")
+    suspend fun postCreateShortGame(
+        @Body request: RequestCreateShortGameDto,
+    ): ResponseCreateShortGameDto
 }
