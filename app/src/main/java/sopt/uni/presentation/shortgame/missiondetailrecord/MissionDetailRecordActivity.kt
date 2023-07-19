445ba3cf40fd3bc0b6b2c5712d5429fd5cdd3796
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-07-19 18:46:03 +0900

    #61 / Add getMissionDetail in MissionDetailRecord

--- app/src/main/java/sopt/uni/presentation/shortgame/missiondetailrecord/MissionDetailRecordActivity.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/missiondetailrecord/MissionDetailRecordActivity.kt
@@ -4,18 +4,20 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
 import sopt.uni.databinding.ActivityMissionDetailRecordBinding
 import sopt.uni.util.binding.BindingActivity
 
-class MissionDetailRecordActivity : BindingActivity<ActivityMissionDetailRecordBinding>(R.layout.activity_mission_detail_record) {
+@AndroidEntryPoint
+class MissionDetailRecordActivity :
+    BindingActivity<ActivityMissionDetailRecordBinding>(R.layout.activity_mission_detail_record) {
     private val viewModel: MissionDetailRecordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.missionDetailReCordViewModel = viewModel
-        viewModel.setMissionDetail()
         setClickListener()
     }
 
@@ -32,6 +34,7 @@ class MissionDetailRecordActivity : BindingActivity<ActivityMissionDetailRecordB
             if (missionId != null) context.startActivity(getIntent(context, missionId))
         }
 
-        private fun getIntent(context: Context, missionId: Int) = Intent(context, MissionDetailRecordActivity::class.java).putExtra(MISSION_ID, missionId)
+        private fun getIntent(context: Context, missionId: Int) =
+            Intent(context, MissionDetailRecordActivity::class.java).putExtra(MISSION_ID, missionId)
     }
 }
