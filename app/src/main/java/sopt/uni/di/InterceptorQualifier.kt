03e4ad82c3d59cb434637432e175f9b029a06db7
HaeTi <t9h9@naver.com>
2023-07-22 01:48:49 +0900

    #83 / 린트 에러

--- app/src/main/java/sopt/uni/di/InterceptorQualifier.kt
+++ app/src/main/java/sopt/uni/di/InterceptorQualifier.kt
@@ -1,7 +1,11 @@
-package sopt.uni.di.qualifier // ktlint-disable filename
+package sopt.uni.di
 
 import javax.inject.Qualifier
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class Logger
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Auth
