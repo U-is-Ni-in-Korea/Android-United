332877693e2e32d7dbde1b2d48e79b50d239affc
l2zh <dlwnsgml0109@naver.com>
2023-09-03 19:45:24 +0900

    #99 / 타이머 fragment 화면 전환

---
+++ app/src/main/java/sopt/uni/presentation/timer/TimerActiveFragment.kt
@@ -0,0 +1,14 @@
+package sopt.uni.presentation.timer
+
+import android.os.Bundle
+import android.view.View
+import sopt.uni.R
+import sopt.uni.databinding.FragmentTimerActiveBinding
+import sopt.uni.util.binding.BindingFragment
+
+class TimerActiveFragment :
+    BindingFragment<FragmentTimerActiveBinding>(R.layout.fragment_timer_active) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
