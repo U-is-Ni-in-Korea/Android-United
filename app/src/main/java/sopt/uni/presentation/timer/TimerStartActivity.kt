b829c578320afe728ab25f177ddfc43fe8ab19dd
l2zh <dlwnsgml0109@naver.com>
2023-09-03 17:46:13 +0900

    #99 / 타이머 뷰 작업 완료

---
+++ app/src/main/java/sopt/uni/presentation/timer/TimerStartActivity.kt
@@ -0,0 +1,12 @@
+package sopt.uni.presentation.timer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sopt.uni.R
+
+class TimerStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_start)
+    }
+}
