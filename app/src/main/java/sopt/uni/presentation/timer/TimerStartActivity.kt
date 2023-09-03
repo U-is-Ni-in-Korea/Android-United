2afb7f5241bfd6bd8cadc8c36ba139fc8bb42274
l2zh <dlwnsgml0109@naver.com>
2023-09-03 19:46:45 +0900

    #99 / 타이머 fragment 화면 전환

--- app/src/main/java/sopt/uni/presentation/timer/TimerStartActivity.kt
+++ app/src/main/java/sopt/uni/presentation/timer/TimerStartActivity.kt
@@ -1,12 +1,28 @@
 package sopt.uni.presentation.timer
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
+import sopt.uni.databinding.ActivityTimerBinding
+import sopt.uni.util.binding.BindingActivity
 
-class TimerStartActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class TimerStartActivity : BindingActivity<ActivityTimerBinding>(R.layout.activity_timer) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer_start)
+        setContentView(binding.root)
+
+        initSetting()
     }
+
+    private fun initSetting() {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTimerSetting = TimerSettingFragment()
+        fragmentTransaction.add(R.id.fcv_timer, fragmentTimerSetting)
+        fragmentTransaction.commit()
+    }
+
 }
