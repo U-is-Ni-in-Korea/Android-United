7b407c6340097a22cb4954fad50f836dd522568a
l2zh <dlwnsgml0109@naver.com>
2023-09-13 18:44:52 +0900

    #99 / 타이머 로직 재변경

--- app/src/main/java/sopt/uni/presentation/timer/TimerSettingFragment.kt
+++ app/src/main/java/sopt/uni/presentation/timer/TimerSettingFragment.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
 import sopt.uni.databinding.FragmentTimerSettingBinding
@@ -47,7 +50,6 @@ class TimerSettingFragment :
             val secondValue = binding.numberpickerSeconds.value
             val total = minuteValue * 60 + secondValue
 
-            viewModel.setPauseState(false)
 
             val sharedPreferences =
                 requireContext().getSharedPreferences("timer_prefs", Context.MODE_PRIVATE)
@@ -55,20 +57,20 @@ class TimerSettingFragment :
             sharedPreferences.edit().putBoolean("isTimerActive", true).apply()
             sharedPreferences.edit().putBoolean("isTimerPause", false).apply()
 
-//            val data = Data.Builder()
-//                .putLong("totalSeconds", total.toLong())
-//                .build()
-//
-//            val timerWorkRequest = OneTimeWorkRequestBuilder<TimerWorker>()
-//                .setInputData(data) // 타이머 시간을 Worker에 전달
-//                .addTag("timer_work_tag") // 태그를 지정
-//                .build()
-//
-//            // WorkManager를 사용하여 Worker를 실행합니다.
-//            val workManager = WorkManager.getInstance(requireContext())
-//            workManager.enqueue(timerWorkRequest)
+            val data = Data.Builder()
+                .putLong("totalSeconds", total.toLong())
+                .build()
 
-            val fragmentTimerActive = TimerActiveFragment(total.toLong())
+            val timerWorkRequest = OneTimeWorkRequestBuilder<TimerWorker>()
+                .setInputData(data) // 타이머 시간을 Worker에 전달
+                .addTag("timer_work_tag") // 태그를 지정
+                .build()
+
+            // WorkManager를 사용하여 Worker를 실행합니다.
+            val workManager = WorkManager.getInstance(requireContext())
+            workManager.enqueue(timerWorkRequest)
+
+            val fragmentTimerActive = TimerActiveFragment(total.toFloat())
             val fragmentTransaction = parentFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fcv_timer, fragmentTimerActive)
             fragmentTransaction.commit()
--- app/src/main/java/sopt/uni/presentation/timer/TimerWorker.kt
+++ app/src/main/java/sopt/uni/presentation/timer/TimerWorker.kt
@@ -1,27 +1,46 @@
-//package sopt.uni.presentation.timer
-//
-//import android.content.Context
-//import android.util.Log
-//import java.util.Timer
-//
-//class TimerWorker(private val context: Context, private val uiUpdateCallback: (Long) -> Unit) {
-//
-//    private var timerTask: Timer? = null
-//    private fun runTimer(totalTime: Long) {
-//        var remainingTime = totalTime // var로 변경
-//        val sharedPreferences =
-//            context.getSharedPreferences("timer_prefs", Context.MODE_PRIVATE)
-//
-//        if (remainingTime >= 0) {
-//            timerTask = kotlin.concurrent.timer(period = 1000) {    // timer() 호출
-//                remainingTime--
-//                Log.d("Timer", "Remaining Seconds: $totalTime")
-//
-//                sharedPreferences.edit().putLong("remainingSeconds", remainingTime).apply()
-//                // UI조작을 위한 메서드
-//                uiUpdateCallback(remainingTime)
-//            }
-//        }
-//
-//    }
-//}
+package sopt.uni.presentation.timer
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class TimerWorker(context: Context, params: WorkerParameters) :
+    Worker(context, params) {
+
+    private var totalSeconds: Long = 0L
+
+    override fun doWork(): Result {
+        val sharedPreferences =
+            applicationContext.getSharedPreferences("timer_prefs", Context.MODE_PRIVATE)
+        val inputData = inputData
+        totalSeconds = inputData.getLong("totalSeconds", 0L)
+        val startTimer = sharedPreferences.getBoolean("isTimerActive", false)
+
+        while (totalSeconds >= 0 && startTimer) {
+            val cancel = sharedPreferences.getBoolean("isTimerActive", true)
+            val pause = sharedPreferences.getBoolean("isTimerPause", false)
+
+            if (!cancel) {
+                break
+            }
+
+            if (pause) {
+                continue
+            }
+
+            Log.d("TimerWorker", "Remaining Seconds: $totalSeconds")
+            Thread.sleep(1000)
+            totalSeconds--
+            sharedPreferences.edit().putLong("remainingSeconds", totalSeconds).apply()
+        }
+
+        sharedPreferences.edit().putBoolean("isTimerActive", false).apply()
+        sharedPreferences.edit().remove("remainingSeconds").apply()
+        sharedPreferences.edit().remove("total_time").apply()
+
+        // Worker가 실행을 완료했을 때 Result.success()를 반환합니다.
+        return Result.success()
+    }
+
+}
