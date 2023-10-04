11121ab62f8447d93e109385d65aa88eb57e6996
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-10-04 19:43:02 +0900

    #102 / 미션카테고리 어댑터 수정

--- app/src/main/java/sopt/uni/presentation/shortgame/createshortgame/MissionViewHolder.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/createshortgame/MissionViewHolder.kt
@@ -1,37 +1,21 @@
 package sopt.uni.presentation.shortgame.createshortgame
 
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import sopt.uni.R
 import sopt.uni.data.entity.shortgame.MissionDetail
 import sopt.uni.databinding.ItemMissionBinding
-import sopt.uni.presentation.entity.MissionIdPosition
 
 class MissionViewHolder(val binding: ItemMissionBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(
         data: MissionDetail,
-        isSelected: Boolean,
-        position: Int,
-        goToMissionDetailClickListener: (MissionIdPosition) -> Unit,
-        selectMissionClickListener: (MissionIdPosition) -> Unit,
+        selectMissionClickListener: (Int) -> Unit,
     ) {
         binding.apply {
-            ivMission.load(data.image)
-            tvMissionTitle.text = data.title
-            ivMissionDetail.setOnClickListener {
-                goToMissionDetailClickListener(MissionIdPosition(data.id, position))
-            }
             root.setOnClickListener {
-                selectMissionClickListener(MissionIdPosition(data.id, position))
+                selectMissionClickListener(data.id)
             }
-        }
-
-        if (isSelected) {
-            binding.clBackground.setBackgroundResource(R.drawable.bg_mission_hover)
-        } else {
-            binding.clBackground.setBackgroundResource(R.drawable.bg_white_round_10)
+            missionDetail = data
         }
     }
 }
