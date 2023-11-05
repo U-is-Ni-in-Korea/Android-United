eb877df6820f9673a915df4e8ca4c86f27743a72
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2023-11-05 22:01:32 +0900

    #101 / lint 수정

--- app/src/main/java/sopt/uni/presentation/memo/MemoViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/memo/MemoViewModel.kt
@@ -1,6 +1,5 @@
 package sopt.uni.presentation.memo
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
