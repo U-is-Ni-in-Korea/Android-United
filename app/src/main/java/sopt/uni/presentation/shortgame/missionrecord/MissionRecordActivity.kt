ca547621d9c58afebc44f3dced2cb326b643e6f1
sub101 <ssubbin10@gmail.com>
2023-11-13 15:24:48 +0900

    #100/불필요한 코드 수정

--- app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordActivity.kt
+++ app/src/main/java/sopt/uni/presentation/shortgame/missionrecord/MissionRecordActivity.kt
@@ -3,24 +3,14 @@ package sopt.uni.presentation.shortgame.missionrecord
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import sopt.uni.R
-import sopt.uni.data.entity.shortgame.GameGuideItem
-import sopt.uni.data.entity.shortgame.MissionToolState
 import sopt.uni.databinding.ActivityMissionRecordBinding
 import sopt.uni.presentation.shortgame.createshortgame.dialog.CreateShortGameDialogFragment
-import sopt.uni.presentation.shortgame.missiondetailrecord.MissionDetailRecordActivity
-import sopt.uni.presentation.memo.MemoActivity
-import sopt.uni.presentation.shortgame.missionresult.MissionResultActivity
-import sopt.uni.presentation.shortgame.missionresult.MissionResultViewModel
 import sopt.uni.presentation.shortgame.missionresult.MissionWaitingResultActivity
-import sopt.uni.presentation.timer.TimerStartActivity
 import sopt.uni.util.binding.BindingActivity
 import sopt.uni.util.extension.setOnSingleClickListener
-import sopt.uni.util.extension.startActivity
 
 @AndroidEntryPoint
 class MissionRecordActivity :
@@ -67,39 +57,6 @@ class MissionRecordActivity :
                 finish()
             }
         }
-        viewModel.myMissionResult.observe(this) {
-            val ruleDescription = it.missionContent.missionCategory.rule
-            val tipDescription = it.missionContent.missionCategory.tip
-            val gameGuideList = listOf<GameGuideItem>(
-                GameGuideItem(
-                    title = getString(R.string.mission_record_rule),
-                    description = ruleDescription,
-                ),
-                GameGuideItem(
-                    title = getString(R.string.mission_record_tip),
-                    description = tipDescription,
-                ),
-            )
-            setAdapter(gameGuideList)
-        }
-        viewModel.missionToolState.observe(this) {
-            when (it) {
-                MissionToolState.MEMO ->
-                    binding.clCardMemo.visibility = View.VISIBLE
-
-                MissionToolState.TIMER ->
-                    binding.clCardTimer.visibility = View.VISIBLE
-
-                else -> {}
-            }
-        }
-    }
-
-    private fun setAdapter(list: List<GameGuideItem>) {
-        binding.vpGameGuide.adapter = GameGuideViewPagerAdapter().apply {
-            submitList(list)
-        }
-        binding.indicator.setViewPager2(binding.vpGameGuide)
     }
 
     private fun setClickListener() {
@@ -113,12 +70,6 @@ class MissionRecordActivity :
             btnMissionFail.setOnSingleClickListener {
                 viewModel.requestMission(false)
             }
-            clCardTimer.setOnSingleClickListener {
-                startActivity<TimerStartActivity>()
-            }
-            clCardMemo.setOnSingleClickListener {
-                startActivity<MemoActivity>()
-            }
         }
     }
 
@@ -129,7 +80,7 @@ class MissionRecordActivity :
             context.startActivity(getIntent(context, roundGameId))
         }
 
-        fun getIntent(context: Context, roundGameId: Int) =
+        private fun getIntent(context: Context, roundGameId: Int) =
             Intent(context, MissionRecordActivity::class.java).putExtra(ROUND_GAME_ID, roundGameId)
     }
 }
