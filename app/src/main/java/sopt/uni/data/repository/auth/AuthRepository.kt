e9c8be4fa286725dc3af11001e2b5f702ce8a1b2
HaeTi <t9h9@naver.com>
2023-12-05 15:51:22 +0900

    #119 / getToken 반환값 변경

--- app/src/main/java/sopt/uni/data/repository/auth/AuthRepository.kt
+++ app/src/main/java/sopt/uni/data/repository/auth/AuthRepository.kt
@@ -1,9 +1,9 @@
 package sopt.uni.data.repository.auth
 
-import sopt.uni.data.entity.auth.Token
+import sopt.uni.data.source.remote.response.ResponseAuthDto
 
 interface AuthRepository {
-    suspend fun getToken(social: String, code: String): Result<Token>
+    suspend fun getToken(social: String, code: String): Result<ResponseAuthDto>
 
     suspend fun initToken(accessToken: String, refreshToken: String)
 
