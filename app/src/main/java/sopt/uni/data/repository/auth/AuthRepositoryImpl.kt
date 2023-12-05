4991b0cf251fff49e07a1d2540e806c2bf4e3d6d
HaeTi <t9h9@naver.com>
2023-12-05 15:51:22 +0900

    #119 / getToken 반환값 변경

--- app/src/main/java/sopt/uni/data/repository/auth/AuthRepositoryImpl.kt
+++ app/src/main/java/sopt/uni/data/repository/auth/AuthRepositoryImpl.kt
@@ -3,14 +3,15 @@ package sopt.uni.data.repository.auth
 import sopt.uni.data.datasource.remote.AuthDatasource
 import sopt.uni.data.entity.auth.Token
 import sopt.uni.data.source.remote.request.RequestAuthDto
+import sopt.uni.data.source.remote.response.ResponseAuthDto
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authDatasource: AuthDatasource,
 ) : AuthRepository {
-    override suspend fun getToken(social: String, code: String): Result<Token> =
+    override suspend fun getToken(social: String, code: String): Result<ResponseAuthDto> =
         kotlin.runCatching {
-            authDatasource.getToken(social, RequestAuthDto(code)).toToken()
+            authDatasource.getToken(social, RequestAuthDto(code))
         }.onSuccess {
             Result.success(it)
         }.onFailure {
