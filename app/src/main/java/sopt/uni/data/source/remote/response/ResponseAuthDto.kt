838aa74d657e723e03b10f31112bd11b60baaaf0
HaeTi <t9h9@naver.com>
2023-12-05 15:51:01 +0900

    #119 / token wrapper 삭제

--- app/src/main/java/sopt/uni/data/source/remote/response/ResponseAuthDto.kt
+++ app/src/main/java/sopt/uni/data/source/remote/response/ResponseAuthDto.kt
@@ -2,7 +2,6 @@ package sopt.uni.data.source.remote.response
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import sopt.uni.data.entity.auth.Token
 
 @Serializable
 data class ResponseAuthDto(
@@ -12,6 +11,4 @@ data class ResponseAuthDto(
     val refreshToken: String?,
     @SerialName("couple_id")
     val coupleId: Int?,
-) {
-    fun toToken(): Token = Token(accessToken = this.accessToken, refreshToken = this.refreshToken)
-}
+)
