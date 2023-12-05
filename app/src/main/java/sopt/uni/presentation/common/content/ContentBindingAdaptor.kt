1c2cca62cf10327260f6e3486e24310ca4a4ef9d
HaeTi <t9h9@naver.com>
2023-12-05 23:08:24 +0900

    #133 / Nickname Error Messagge BindingAdapter 삭제

--- app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
+++ app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
@@ -1,19 +1,8 @@
 package sopt.uni.presentation.common.content
 
-import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import sopt.uni.R
-
-@BindingAdapter("nicknameErrorMessageVisibility")
-fun setNicknameErrorMessageVisibility(view: TextView, length: Int) {
-    if (length in 0..MAX_LENGTH) {
-        view.visibility = View.INVISIBLE
-    } else {
-        view.visibility = View.VISIBLE
-    }
-}
 
 @BindingAdapter("nicknameFieldStrokeColor")
 fun setNicknameFieldStrokeColor(view: EditText, length: Int) {
