635def159b0dbeeffa981f2cc18e91db1189eec3
HaeTi <t9h9@naver.com>
2023-12-06 17:07:35 +0900

    #137 / 반환 타입을 Response로 감싸기

--- app/src/main/java/sopt/uni/data/service/HomeService.kt
+++ app/src/main/java/sopt/uni/data/service/HomeService.kt
@@ -1,10 +1,11 @@
 package sopt.uni.data.service
 
+import retrofit2.Response
 import retrofit2.http.GET
 import sopt.uni.data.source.remote.response.ResponseHomeDto
 
 interface HomeService {
 
     @GET("api/home")
-    suspend fun getHome(): ResponseHomeDto
+    suspend fun getHome(): Response<ResponseHomeDto>
 }
