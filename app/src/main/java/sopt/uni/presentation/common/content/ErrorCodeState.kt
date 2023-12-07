b2a7ee873695490defb22bd0fe78769b2f3e9b2f
HaeTi <t9h9@naver.com>
2023-12-07 12:49:37 +0900

    #128 / No Partner state type 추가

--- app/src/main/java/sopt/uni/presentation/common/content/ErrorCodeState.kt
+++ app/src/main/java/sopt/uni/presentation/common/content/ErrorCodeState.kt
@@ -1,6 +1,7 @@
 package sopt.uni.presentation.common.content
 
 sealed class ErrorCodeState {
+    object NoPartner : ErrorCodeState()
     object NoError : ErrorCodeState()
     object NoToken : ErrorCodeState()
 }
