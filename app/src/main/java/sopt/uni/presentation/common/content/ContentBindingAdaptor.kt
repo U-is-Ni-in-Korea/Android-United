1287919bf6f710038fe97286085a243f08dbf0fd
HaeTi <t9h9@naver.com>
2023-12-09 14:52:14 +0900

    #147 / bindingAdaptor로 strokeColor 조정

--- app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
+++ app/src/main/java/sopt/uni/presentation/common/content/ContentBindingAdaptor.kt
@@ -14,3 +14,12 @@ fun setNicknameFieldStrokeColor(view: EditText, length: Int) {
         view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text)
     }
 }
+
+@BindingAdapter("inviteCodeFieldStrokeColor")
+fun setInviteCodeFieldStrokeColor(view: EditText, length: Int) {
+    if (length > 0) {
+        view.background = view.context.getDrawable(R.drawable.bg_mypage_edit_text)
+    } else {
+        view.background = view.context.getDrawable(R.drawable.bg_enter_edit_text_empty)
+    }
+}
