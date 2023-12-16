01d9fa3818ad64f79915bfcea1be2ed100c87298
HaeTi <t9h9@naver.com>
2023-12-16 10:42:12 +0900

    #180 / Backing property on inviteCode

--- app/src/main/java/sopt/uni/presentation/invite/DdayViewModel.kt
+++ app/src/main/java/sopt/uni/presentation/invite/DdayViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import sopt.uni.data.repository.onboarding.OnBoardingRepository
 import timber.log.Timber
@@ -14,14 +15,15 @@ class DdayViewModel @Inject constructor(
     private val onBoardingRepository: OnBoardingRepository,
 ) : ViewModel() {
 
-    var inviteCode = MutableStateFlow("")
+    private val _inviteCode = MutableStateFlow<String>("")
+    val inviteCode get() = _inviteCode.asStateFlow()
 
     fun postStartDate(startDate: String) {
         viewModelScope.launch {
             kotlin.runCatching {
                 onBoardingRepository.postStartDate(startDate = startDate)
             }.fold({
-                inviteCode.emit(it)
+                _inviteCode.emit(it)
                 Timber.d("기념일 갱신이 완료되었습니다.")
             }, {
                 Timber.d("기념일 갱신이 실패했습니다.")
