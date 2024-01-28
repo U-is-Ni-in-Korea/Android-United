4fb0c97bbc9f0dbf78e24a113d00b221cedad45e
l2zh <dlwnsgml0109@naver.com>
2024-01-29 01:32:52 +0900

    #121 / 업데이트 다이얼로그 콜백 추가

--- app/src/main/java/sopt/uni/presentation/home/UpdateDialogFragment.kt
+++ app/src/main/java/sopt/uni/presentation/home/UpdateDialogFragment.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateOptions
@@ -15,11 +14,14 @@ import sopt.uni.data.datasource.local.SparkleStorage
 import sopt.uni.databinding.TitleAction2DialogBinding
 import sopt.uni.util.binding.BindingDialogFragment
 import sopt.uni.util.extension.setOnSingleClickListener
+import sopt.uni.util.extension.showSnackbar
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class UpdateDialogFragment :
     BindingDialogFragment<TitleAction2DialogBinding>(R.layout.title_action2_dialog) {
+
+    lateinit var updateDialogListener: UpdateDialogListener
 
     @Inject
     lateinit var appUpdateManager: AppUpdateManager
@@ -34,6 +36,7 @@ class UpdateDialogFragment :
             btnRight.setText(getString(R.string.update_dialog_ok))
             btnLeft.setOnSingleClickListener {
                 dismiss()
+                updateDialogListener.onDialogDismissed()
             }
             btnRight.setOnSingleClickListener {
                 updateSparkle()
@@ -52,18 +55,23 @@ class UpdateDialogFragment :
     private val startForResult =
         registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
             if (it.resultCode != Activity.RESULT_OK) {
-                Toast.makeText(requireContext(), "업데이트에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT)
-                    .show()
+                showSnackbar(binding.root, "업데이트에 실패했습니다. 다시 시도해주세요.")
                 Log.e("update", it.resultCode.toString())
             } else {
                 SparkleStorage.setUpdateAvailableBoolean(
                     IS_UPDATE_AVAILABLE,
                     true,
                 )
+                updateDialogListener.onUpdateComplete()
             }
         }
 
     companion object {
         const val IS_UPDATE_AVAILABLE = "IS_UPDATE_AVAILABLE"
+    }
+
+    interface UpdateDialogListener {
+        fun onUpdateComplete()
+        fun onDialogDismissed()
     }
 }
